5bf3e0fdee7e07659289b7f08911bb577807b360
zhangws <zhang.wetts@163.com>
2016-12-09 10:29:25 +0800

    添加swagger2

---
+++ src/main/java/com/wetts/restful/demo/MySwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.wetts.restful.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by wetts on 2016/12/8.
+ */
+@Configuration
+@EnableWebMvc //NOTE: Only needed in a non-springboot application
+@EnableSwagger2
+public class MySwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SpringMVC 中使用Swagger2构建RESTful APIs")
+                .description("SpringMVC 中使用Swagger2构建RESTful APIs")
+                .termsOfServiceUrl("https://wetts.github.io/")
+                .contact(new Contact("wetts", "https://wetts.github.io/", "zhang.wetts@163.com"))
+                .version("1.0")
+                .build();
+    }
+}
--- src/main/java/com/wetts/restful/demo/controller/EmployeeController.java
+++ src/main/java/com/wetts/restful/demo/controller/EmployeeController.java
@@ -3,50 +3,61 @@ package com.wetts.restful.demo.controller;
 import com.wetts.restful.demo.bean.Employee;
 import com.wetts.restful.demo.bean.EmployeeList;
 import com.wetts.restful.demo.ds.EmployeeDS;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api")
+// 该注解，用在类上，说明该类的作用
+@Api(value = "/api", description = "用户的相关操作")
 public class EmployeeController {
 
-	private EmployeeDS employeeDS = new EmployeeDS();
+    private EmployeeDS employeeDS = new EmployeeDS();
 
-	@RequestMapping(method=RequestMethod.GET, value="/emp/{id}", headers="Accept=application/xml, application/json")
-	public @ResponseBody Employee getEmp(@PathVariable String id) {
-		Employee e = employeeDS.get(Long.parseLong(id));
-		return e;
-	}
+    // 该注解，用在方法上，说明该类的作用
+    @ApiOperation(value = "获取用户详细信息", notes = "根据url的id来获取用户详细信息")
+    // 用在方法上包含一个参数说明
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "String", paramType="path", defaultValue="1")
+    @RequestMapping(method = RequestMethod.GET, value = "/emp/{id}", headers = "Accept=application/xml, application/json")
+    public Employee getEmp(@PathVariable(value = "id") String id) {
+        Employee e = employeeDS.get(Long.parseLong(id));
+        return e;
+    }
 
-	@RequestMapping(method=RequestMethod.GET, value="/emps", headers="Accept=application/xml, application/json")
-	public @ResponseBody EmployeeList getAllEmp() {
-		List<Employee> employees = employeeDS.getAll();
-		EmployeeList list = new EmployeeList(employees);
-		return list;
-	}
+    @ApiOperation(value = "获取用户列表", notes = "")
+    @RequestMapping(method = RequestMethod.GET, value = "/emps", headers = "Accept=application/xml, application/json")
+    public EmployeeList getAllEmp() {
+        List<Employee> employees = employeeDS.getAll();
+        EmployeeList list = new EmployeeList(employees);
+        return list;
+    }
 
-	@RequestMapping(method=RequestMethod.POST, value="/emp")
-	public @ResponseBody Employee addEmp(@RequestBody Employee e) {
-		employeeDS.add(e);
-		return e;
-	}
+    @ApiOperation(value = "创建用户", notes = "根据Emp对象创建用户")
+    @RequestMapping(method = RequestMethod.POST, value = "/emp")
+    public Employee addEmp(@RequestBody Employee e) {
+        employeeDS.add(e);
+        return e;
+    }
 
-	@RequestMapping(method=RequestMethod.PUT, value="/emp/{id}")
-	public @ResponseBody Employee updateEmp(@RequestBody Employee e, @PathVariable String id) {
-		employeeDS.update(e);
-		return e;
-	}
+    @ApiOperation(value = "更新用户详细信息", notes = "根据url的id来指定更新对象，并根据传过来的emp信息来更新用户详细信息")
+    // 用在方法上包含一组参数说明
+    @ApiImplicitParams({@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "String", paramType="path"),
+            @ApiImplicitParam(name = "e", value = "用户详细实体emp", required = true, dataType = "Employee")})
+    @RequestMapping(method = RequestMethod.PUT, value = "/emp/{id}")
+    public Employee updateEmp(@RequestBody Employee e, @PathVariable(value = "id") String id) {
+        employeeDS.update(e);
+        return e;
+    }
 
-	@RequestMapping(method=RequestMethod.DELETE, value="/emp/{id}")
-	public @ResponseBody void removeEmp(@PathVariable String id) {
-		employeeDS.remove(Long.parseLong(id));
-	}
+    @ApiOperation(value = "删除用户", notes = "根据url的id来指定删除对象")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/emp/{id}")
+    public void removeEmp(@PathVariable(value = "id") String id) {
+        employeeDS.remove(Long.parseLong(id));
+    }
 
-	@RequestMapping(method=RequestMethod.GET, value="/hello")
-	public String helloWorld() {
-		return "index";
-	}
 }
